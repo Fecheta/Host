d0b9832648c46e1085ade539574f4cf62c363eed
Fecheta Virgil-Vicentiu <75781128+Fecheta@users.noreply.github.com>
2021-05-27 22:58:39 +0300

    txt21

--- web/app/views/vizitator/viziteaza.php
+++ web/app/views/vizitator/viziteaza.php
@@ -64,7 +64,7 @@
         <div class="line">
             <label for="rude">Relatia cu detinutul</label>
             <select name="grad" id="rude" required>
-                <option label="select"> - - select - - </option>
+                <option value=""> - - select - - </option>
                 <option value="0">Ruda</option>
                 <option value="1">Tutore legal</option>
                 <option value="2">Avocat</option>
@@ -75,7 +75,7 @@
         <div class="line">
             <label for="scop">Scopul Vizitei</label>
             <select name="grad" id="scop" required>
-                <option label="select"> - - select - - </option>
+                <option value=""> - - select - - </option>
                 <option value="0">Vizita Amicala</option>
                 <option value="1">Chestiuni Judecatresti</option>
                 <option value="2">Probleme Personale</option>
