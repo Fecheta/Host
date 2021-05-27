5d6c9b5592bca9818848b1b9c75fb763b017961d
Fecheta Virgil-Vicentiu <75781128+Fecheta@users.noreply.github.com>
2021-05-27 14:40:30 +0300

    txt6

--- web/app/views/startPages/index.php
+++ web/app/views/startPages/index.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="DeMo tool, util la inchisoare">
     <title>DeMO</title>
     <link rel="stylesheet" href="/public/css/startPages/p1.css">
     <link rel="stylesheet" href="css/normalize.css" type="test/css" media="screen">
