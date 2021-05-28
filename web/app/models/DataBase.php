8b466ee07e7f4b262f890d3adf82d59203dca208
Fecheta Virgil-Vicentiu <75781128+Fecheta@users.noreply.github.com>
2021-05-28 23:20:33 +0300

    c28

--- web/app/models/DataBase.php
+++ web/app/models/DataBase.php
@@ -1,15 +1,15 @@
 <?php
 
 $cleardb_url = parse_url(getenv("mysql://b2d8f581f3e0f5:f8a7a47e@us-cdbr-east-03.cleardb.com/heroku_08132accc9fc692?reconnect=true"));
-$cleardb_server = $cleardb_url["host"];
-$cleardb_username = $cleardb_url["user"];
-$cleardb_password = $cleardb_url["pass"];
-$cleardb_db = "heroku_08132accc9fc692";
+// $cleardb_server = $cleardb_url["host"];
+// $cleardb_username = $cleardb_url["user"];
+// $cleardb_password = $cleardb_url["pass"];
+// $cleardb_db = "heroku_08132accc9fc692";
 
-    // $cleardb_server = "us-cdbr-east-03.cleardb.com";
-    // $cleardb_username = "b2d8f581f3e0f5";
-    // $cleardb_password = "f8a7a47e";
-    // $cleardb_db = "heroku_08132accc9fc692";
+    $cleardb_server = "us-cdbr-east-03.cleardb.com";
+    $cleardb_username = "b2d8f581f3e0f5";
+    $cleardb_password = "f8a7a47e";
+    $cleardb_db = "heroku_08132accc9fc692";
 
     $conn = mysqli_connect($cleardb_server, $cleardb_username, $cleardb_password, $cleardb_db);
 
