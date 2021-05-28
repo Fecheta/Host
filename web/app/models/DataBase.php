d759a3b471bef6b95ff180a0cd7123e0ae5c2b4b
Fecheta Virgil-Vicentiu <75781128+Fecheta@users.noreply.github.com>
2021-05-28 20:16:09 +0300

    c8

--- web/app/models/DataBase.php
+++ web/app/models/DataBase.php
@@ -1,6 +1,6 @@
 <?php
 
-$cleardb_url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+$cleardb_url = parse_url(getenv("mysql://b2d8f581f3e0f5:f8a7a47e@us-cdbr-east-03.cleardb.com/heroku_08132accc9fc692?reconnect=true"));
 $cleardb_server = $cleardb_url["host"];
 $cleardb_username = $cleardb_url["user"];
 $cleardb_password = $cleardb_url["pass"];
