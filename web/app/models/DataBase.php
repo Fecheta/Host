dbd8a9704859f475793cac70dcee61d776e16c89
Fecheta Virgil-Vicentiu <75781128+Fecheta@users.noreply.github.com>
2021-05-28 22:21:49 +0300

    c18

--- web/app/models/DataBase.php
+++ web/app/models/DataBase.php
@@ -20,7 +20,7 @@ $conn = mysqli_connect($cleardb_server, $cleardb_username, $cleardb_password, $c
     echo "Conected!";
 
     function getDetinut($idDetinut){
-        $sql = "SELECT id_detinut, nume, prenume FROM detinuti WHERE id_detinut = " . $idDetinut;
+        $sql = "SELECT * FROM detinuti";
         $result = mysqli_query($sql);
 
         $row = mysqli_fetch_assoc($result);
