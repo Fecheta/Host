4b02f3dfa7dcd6534ba0e59b7e0de0daaadf9a73
Fecheta Virgil-Vicentiu <75781128+Fecheta@users.noreply.github.com>
2021-05-28 23:22:08 +0300

    c29

--- web/app/models/DataBase.php
+++ web/app/models/DataBase.php
@@ -22,6 +22,6 @@ $cleardb_url = parse_url(getenv("mysql://b2d8f581f3e0f5:f8a7a47e@us-cdbr-east-03
         $result = mysqli_query($conn, $sql);
 
         $row = mysqli_fetch_assoc($result);
-        echo $row["nume"];
+        echo $row["id_detinut"] . " " . $row["nume"] . " " . $row["prenume"];
         // return $row;
 ?>
