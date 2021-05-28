a021cfff60770d3898cde77e1593192c39eed73f
Fecheta Virgil-Vicentiu <75781128+Fecheta@users.noreply.github.com>
2021-05-28 20:04:16 +0300

    c6

--- web/app/models/DataBase.php
+++ web/app/models/DataBase.php
@@ -1,10 +1,10 @@
 <?php
 
     $url = parse_url(getenv("mysql://b2d8f581f3e0f5:f8a7a47e@us-cdbr-east-03.cleardb.com/heroku_08132accc9fc692?reconnect=true"));
-    $server = $url["host"];
-    $username = $url["user"];
-    $password = $url["pass"];
-    $db = substr($url["path"], 1);
+    $server = "us-cdbr-east-03.cleardb.com";
+    $username = "b2d8f581f3e0f5";
+    $password = "f8a7a47e";
+    $db = "DBdemo";
     $conn = new mysqli($server, $username, $password, $db);
 
     if($conn->connect_error){
