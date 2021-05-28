5bc4541782c94e816261a42581aa002fd6b4646e
Fecheta Virgil-Vicentiu <75781128+Fecheta@users.noreply.github.com>
2021-05-28 19:43:10 +0300

    c4

--- web/app/models/DataBase.php
+++ web/app/models/DataBase.php
@@ -7,8 +7,6 @@
     $db = substr($url["path"], 1);
     $conn = new mysqli($server, $username, $password, $db);
 
-    $conn = new mysqli($servername, $username, $password, $dbname);
-
     if($conn->connect_error){
         die("Conectare esuata..." . $conn->connect_error);
     }
